053b714b8db554a0fee37fdfa1e0da0c7fdc0ac0
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-10-24 09:27:01 -0300

    adding adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	output: 'hybrid',
-	site: 'https://DaveAdbeel.github.io'
+	output: 'server',
+	site: 'https://DaveAdbeel.github.io',
+	adapter: vercel()
 })
