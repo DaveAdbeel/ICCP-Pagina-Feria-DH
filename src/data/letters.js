52cba68931b30401cb15c49cc9c10a0a8a06bb25
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-10-24 11:55:04 -0300

    adding content to the letters

--- src/data/letters.js
+++ src/data/letters.js
@@ -1,14 +1,17 @@
 export const letters = [
   {
     id: 'carta-1',
-    title: 'Carta 1',
+    title: 'La Sonia Riveros fue de las primeras presas políticas',
     img: "../../public/portada-1.jpg",
-    opinion: 'Una carta de una mujer en la carcel durante la dictadura',
+    opinion: `Según el texto, el autor es M.O. 
+
+El texto hace referencia a la experiencia de una mujer, quien estuvo 11 años en prisión en dos períodos. Cuenta que cuando llegó en 1978 ya había 4 prisioneras políticas, incluyendo a Sonia Riveros de la VOP, quién estaba presa desde el golpe de Estado de 1973 y recién pudo irse al exilio en 1982.  Luego cuando las trasladaron a la cárcel de hombres de San Miguel, eran 14 mujeres y 2 de ellas con sus hijos. 
+`,
     author: 'Carmen Berenguer'
   },
   {
     id: 'carta-2',
-    title: 'Carta 2',
+    title: 'Entre todas construimos la casa de tortura',
     img: "../../public/portada-2.jpg",
     opinion: ` 
 Se trata del testimonio de Carmen Berenguer sobre cuando fue detenida junto a su hijo de 2 años y un hermano. Describe cómo iba con los ojos vendados y desorientada, pero utilizaba sus otros sentidos para tratar de identificar el lugar. 
@@ -20,14 +23,14 @@ Cuando llegó a la cárcel, narró los detalles de su detención para reconstrui
   },
   {
     id: 'carta-3',
-    title: 'Carta 3',
+    title: 'Teníamos una experiencia acumulada',
     img: "../../public/portada-3.png",
     opinion: 'Una carta de una mujer en la carcel durante la dictadura',
     author: 'Carmen Berenguer'
   },
   {
     id: 'carta-4',
-    title: 'Carta 4',
+    title: 'Las vendas eran de la línea aérea de aviación chilena, la LAN',
     img: "../../public/portada-4.jpg",
     opinion: 'Una carta de una mujer en la carcel durante la dictadura',
     author: 'Carmen Berenguer'
