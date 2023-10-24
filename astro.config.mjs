478ef0e0d5c3fd75d8d31fdd4fcbbc095b3cb9fa
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-10-24 09:21:37 -0300

    fixing settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	output: 'server',
-	site: 'https://DaveAdbeel.github.io',
-	base: '/ICCP-Pagina-Feria-DH'
+	site: 'https://DaveAdbeel.github.io'
 })
