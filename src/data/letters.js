0d2eb70174ec386d9951c1e4886fb014fd43e757
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-10-24 11:37:56 -0300

    adding current progress

---
+++ src/data/letters.js
@@ -0,0 +1,35 @@
+export const letters = [
+  {
+    id: 'carta-1',
+    title: 'Carta 1',
+    img: "../../public/portada-1.jpg",
+    opinion: 'Una carta de una mujer en la carcel durante la dictadura',
+    author: 'Carmen Berenguer'
+  },
+  {
+    id: 'carta-2',
+    title: 'Carta 2',
+    img: "../../public/portada-2.jpg",
+    opinion: ` 
+Se trata del testimonio de Carmen Berenguer sobre cuando fue detenida junto a su hijo de 2 años y un hermano. Describe cómo iba con los ojos vendados y desorientada, pero utilizaba sus otros sentidos para tratar de identificar el lugar. 
+
+Por el sonido de un portón pesado, pensó que era una casa grande, específicamente la casa de tortura de la calle Borgoño. Luego, por otros sonidos como de agua en un subterráneo, lo confirmó. 
+
+Cuando llegó a la cárcel, narró los detalles de su detención para reconstruir lo sucedido. Según ella, el 90% de las mujeres detenidas pasaron por esa casa de tortura.`,
+    author: 'Carmen Berenguer'
+  },
+  {
+    id: 'carta-3',
+    title: 'Carta 3',
+    img: "../../public/portada-3.png",
+    opinion: 'Una carta de una mujer en la carcel durante la dictadura',
+    author: 'Carmen Berenguer'
+  },
+  {
+    id: 'carta-4',
+    title: 'Carta 4',
+    img: "../../public/portada-4.jpg",
+    opinion: 'Una carta de una mujer en la carcel durante la dictadura',
+    author: 'Carmen Berenguer'
+  }
+]
